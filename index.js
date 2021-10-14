02b6b5f1961f3a1fd27c20699223b04744730af6
alexander.Khatamov <alexk2120@gmail.com>
2021-10-14 11:59:20 -0400

    phase-1 update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
-    
 function updateEmployeeWithKeyAndValue(obj, key, value) {
         return Object.assign({}, obj, { [key] : value});
     }
-    const employee = {name: 'Sam', streetAddress: '12 Broadway'}
+    const employee = {
+        name: 'Sam', 
+        streetAddress: '12 Broadway'}
     function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
         employee[key] = value;
         return employee;
@@ -12,6 +12,7 @@ function updateEmployeeWithKeyAndValue(obj, key, value) {
         return Object.assign({},obj, {[key] : value});
     }
     function destructivelyDeleteFromEmployeeByKey(employee, key) {
-        return employee[key];
+        delete employee[key]
+        return employee
     
     }
